32d5229f8dc4c73d758a1f6c26d0f70b38aca281
jasonhdev <jasonhdev@gmail.com>
2024-07-02 11:38:30 -0700

    Setup base Add Candidate functionality - implemented POST method, wired to form button.

--- client/app/controllers/candidates.js
+++ client/app/controllers/candidates.js
@@ -1,10 +1,60 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class CandidatesController extends Controller {
+  @tracked showCandidateForm = false;
+  @tracked candidate = {
+    'name': '',
+    'age': ''
+  }
 
   @action
-  addNew () {
-    alert('Not implemented')
+  addNew() {
+    this.showCandidateForm = true;
+  }
+
+  @action
+  submitCandidate() {
+
+    // TODO: Remove after testing
+    if (!this.candidate.name) {
+      this.candidate.name = "Tester";
+    }
+    if (!this.candidate.age) {
+      this.candidate.age = 12;
+    }
+
+    if (!this.candidate.name) {
+      alert('Name is required');
+      return;
+    }
+    if (!this.candidate.age) {
+      alert('Age is required');
+      return;
+    }
+
+    let newCandidate = this.store.createRecord('applicant', {
+      name: this.candidate.name,
+      age: this.candidate.age,
+    });
+
+    newCandidate.save().then(() => {
+      this.model = this.store.query('applicant', {});
+    }).catch((e) => {
+      alert(e);
+      console.log(e);
+    });
+
+    this.clearForm();
+  }
+
+  @action
+  clearForm() {
+    this.candidate = {
+      'name': '',
+      'age': ''
+    }
+    this.showCandidateForm = false;
   }
 }
--- server/public/index.php
+++ server/public/index.php
@@ -4,44 +4,46 @@ use Phalcon\Loader;
 use Phalcon\Mvc\Micro;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
+use Phalcon\Http\Response;
+use App\Models\Candidates;
 
 $loader = new Loader();
 $loader->registerNamespaces(
-    [
-        'App\Models' => __DIR__ . '/models/',
-    ]
+  [
+    'App\Models' => __DIR__ . '/models/',
+  ]
 );
 $loader->register();
 
 
 $container = new FactoryDefault();
 $container->set(
-    'db',
-    function () {
-        return new PdoMysql(
-            [
-                'host'     => 'db',
-                'username' => 'dev',
-                'password' => 'plokijuh',
-                'dbname'   => 'hiring',
-            ]
-        );
-    }
+  'db',
+  function () {
+    return new PdoMysql(
+      [
+        'host'     => 'db',
+        'username' => 'dev',
+        'password' => 'plokijuh',
+        'dbname'   => 'hiring',
+      ]
+    );
+  }
 );
 
 $app = new Micro($container);
 
 $app->get(
-    '/',
-    function () {
-      header('Content-type: application/json');
-      echo json_encode([
-        'available REST endpoints:',
-        'GET /api/applicants',
-        'GET /api/applicants/{id}',
-        'POST /api/applicants',
-      ]);
-    }
+  '/',
+  function () {
+    header('Content-type: application/json');
+    echo json_encode([
+      'available REST endpoints:',
+      'GET /api/applicants',
+      'GET /api/applicants/{id}',
+      'POST /api/applicants',
+    ]);
+  }
 );
 
 $app->get(
@@ -50,8 +52,7 @@ $app->get(
     $phql = "SELECT id, name, age FROM App\Models\Candidates ORDER BY age";
     $candidates = $app
       ->modelsManager
-      ->executeQuery($phql)
-    ;
+      ->executeQuery($phql);
 
     $data = [];
 
@@ -60,14 +61,58 @@ $app->get(
         'type' => 'applicant',
         'id'   => $cand->id,
         'attributes' => [
-        'name' => $cand->name,
-        'age' => $cand->age,
-      ]
+          'name' => $cand->name,
+          'age' => $cand->age,
+        ]
       ];
     }
 
     header('Content-type: application/vnd.api+json'); // JSON API
     echo json_encode(['data' => $data]);
+  }
+);
+
+$app->post(
+  '/api/applicants',
+  function () use ($app) {
+
+    $payload = $app->request->getJsonRawBody();
+
+    $response = new Response();
+
+    try {
+      $candidate = new Candidates();
+      $candidate->name = $payload->data->attributes->name;
+      $candidate->age = $payload->data->attributes->age;
+
+      if ($candidate->create()) {
+        $response
+          ->setStatusCode(201)
+          ->setJsonContent([
+            'data' => [
+              'type' => 'applicant',
+              'attributes' => $candidate->toArray([
+                'name',
+                'age',
+              ]),
+            ]
+          ]);
+      } else {
+        $response
+          ->setStatusCode(422)
+          ->setJSONContent([
+            'errors' => $candidate->getMessages()
+          ]);
+      }
+    } catch (Exception $e) {
+      $response
+        ->setStatusCode(400)
+        ->setJSONContent([
+          'errors' => $e->getMessage()
+        ]);
+    }
+
+    return $response;
   }
 );
 
